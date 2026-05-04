0edf477f2f98fa06f9e14153e405fdff96dc7661
LuisGabriel <bielito0606@gmail.com>
2026-05-04 08:54:33 -0300

    concluido a tela de abertura

--- LuisShoppingList/app/_layout.jsx
+++ LuisShoppingList/app/_layout.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Stack } from 'expo-router'
-import { StatusBar } from 'expo-status-bar'
 
 export default function RootLayout() {
   return (
     <>
       <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }}/>
-        <Stack.Screen name="home" options={{headerShown: false}}/>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
       </Stack>
-      <Status></Status>
     </>
   )
 }
--- LuisShoppingList/app/index.jsx
+++ LuisShoppingList/app/index.jsx
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
+export default function Welcome() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.containerImage}>
+        <Image 
+          source={require('../assets/avatar.png')}
+          style={styles.avatar}
+        />
+      </View>
+      <View style={styles.content}>
+        <Text style={styles.title}>Luis Shopping List</Text>
+        <Text style={styles.text}>
+          Monte sua lista de compras e não esqueça mais o que comprar ao sair de casa!!!
+        </Text>
+        <Link style={styles.button} href={"/home"}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </Link>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
   },
+  containerImage:{
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 230,
+    height: 230,
+    borderRadius: 115,
+    resizeMode: 'cover',
+  },
+  content: {
+    flex: 1,
+    marginHorizontal: 5,
+    backgroundColor: '#dadada',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingHorizontal: '5%',
+  },
+  title: {
+    fontSize: 30,
+    fontWeigth: 'bold',
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+  text: {}
 });
